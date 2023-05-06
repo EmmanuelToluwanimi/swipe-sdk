f5b4accb040c1454a6c73ce2251c7596be36246d
Emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2023-05-06 15:39:05 +0100

    feat: interfaces

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -6,3 +6,11 @@ export interface IPagination {
     limit: number;
     offset: number;
 }
+
+export interface IFetcher {
+    url: string;
+    method: string;
+    apiKey: string;
+    body?: any;
+    pagination?: IPagination
+}
